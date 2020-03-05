0a0c97a6d75993aee21d0e311dc4d0b438874e50
Nate Arnold <nate.arnold@equipmentshare.com>
2020-03-05 13:53:31 -0600

    Update language

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,10 +18,12 @@ const IndexPage = () => (
         <h1>Become a Sovereign Cyclist!</h1>
         <p>
           A <b>Sovereign Cyclist</b> is a cyclist who is equipped with the
-          kownledge, resources and support he/she needs to grow and thrive as a
-          training athlete. Becoming a stronger cyclsist is all about training
-          the body to be stronger, longer and more efficient. We are building a
-          community to inspire and learn as we pursue these goals together.
+          knowledge, resources and support he/she needs to grow and thrive as a
+          training athlete. Becoming a stronger cyclist is all about training
+          the body to be stronger, longer and more efficient, while at the same
+          time, knowing when to back off. We are building a community to inspire
+          and learn as we pursue these goals together. Sign up to follow along
+          and get more information as it becomes available!
         </p>
         <Signup />
       </div>
